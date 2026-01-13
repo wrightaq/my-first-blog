04424925dd4b3fa9342895fb2612f641a5093a25
Amanda <amandaqwright@gmail.com>
2026-01-13 15:52:41 -0600

    Modified templates to display posts from database.

--- Django_Notes.py
+++ Django_Notes.py
@@ -1,4 +1,9 @@
 # username: wrightaq password: Django
+# create venv: python3.12 -m venv myvenv
 # start venv:source myvenv/bin/activate
 # to start server: python manage.py runserver
 # local database is different than database on PythonAnywhere
+# to exit python shell type exit()
+
+
+#views connect models and templates so that our posts appear in our HTML template
