12fb2b28b8aeb2b86e0b09997aff6ca2a6588ebd
Amanda <amandaqwright@gmail.com>
2026-01-14 11:52:54 -0600

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'), #making a list of all posts the root view when visited
     path('post/<int:pk>', views.post_detail, name='post_detail'), #to show the post details of a post specified by its pk (or primary key)
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
