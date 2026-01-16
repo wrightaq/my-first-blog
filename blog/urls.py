7cb8f77c7c1e0ecd2ac5b737967549cbde6bed4f
Amanda <amandaqwright@gmail.com>
2026-01-15 20:36:51 -0600

    changed urlpatterns order

--- blog/urls.py
+++ blog/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('', views.post_list, name='post_list'), #making a list of all posts the root view when visited
     path('post/<int:pk>', views.post_detail, name='post_detail'), #to show the post details of a post specified by its pk (or primary key)
     path('post/new/', views.post_new, name='post_new'),
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('drafts/', views.post_draft_list, name='post_draft_list'),#for unpublished posts
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
     path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
 ]
